359985edd08cb697641a7f01b06cc16a5eb36bad
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-12-08 11:13:40 -0500

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+class Rectangle {
+public:
+	Rectangle(double w,double h);
+	int get_perimeter() const;
+	int get_area() const;
+	void resize(double factor);
+private:
+	double width;
+	double height;
+};
+
+Rectangle::Rectangle(double w, double h) {
+	width = w;
+	height = h;
+}
+
+int Rectangle::get_perimeter() const {
+	return (width * 2) + (height * 2);
+}
+
+int Rectangle::get_area() const {
+	return height * width;
+}
+
+void Rectangle::resize(double factor) {
+	height *= 2;
+	width *= 2;
+}
